908086979e207c9e5b174796ed0d23717ada1949
Selene <spachecor03@gmail.com>
2025-01-13 10:28:02 +0100

    iniciado tema 4 spring microservicios

---
+++ PSP/Tema4/MiPrimerMicroServicio/src/test/java/com/spachecor/miprimermicroservicio/MiPrimerMicroServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spachecor.miprimermicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerMicroServicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
